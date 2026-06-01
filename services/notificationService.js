b1873878d80c6954172010fd03defad33255f7c5
indhpw <indahpurnaningwulan@gmail.com>
2026-06-01 11:29:14 +0700

    use firebase env variables

--- services/notificationService.js
+++ services/notificationService.js
@@ -11,9 +11,11 @@ require('dotenv').config();
 // Inisialisasi Firebase Admin (hanya sekali)
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(
-      require('../config/appmia-e39ce-firebase-adminsdk-fbsvc-f8cebe06cb.json')
-    ),
+    credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n')    
+    }),
   });
 }
 
